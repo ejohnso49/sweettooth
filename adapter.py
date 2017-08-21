be5b7c95740bead250a3c4d77d5f5681a4343628
Eric Johnson <eric.j.johnson22@gmail.com>
2017-08-20 17:18:10 -0700

    First cut at Adapter class using pydbus
    -This class has access to all of the specified HCI's properties
    -Will be adding an event loop to process signals obtained

--- adapter.py
+++ adapter.py
@@ -18,6 +18,111 @@
 from gi.repository import GLib
 import pydbus
 
+BLUEZ_NAME = 'org.bluez'
+SYSTEM_BUS = pydbus.SystemBus()
 
-BLUEZ_NAME = "org.bluez"
-bus = pydbus.SystemBus()
+
+class Adapter (object):
+    __bluez_root = SYSTEM_BUS.get(BLUEZ_NAME, '/')
+
+    def __init__(self, hci_address):
+        self._adapter_obj = self._find_adapter(hci_address)
+        self.powered = True
+
+    @property
+    def uuids(self):
+        return self._adapter_obj.UUIDs
+
+    @property
+    def discoverable(self):
+        return self._adapter_obj.Discoverable
+
+    @discoverable.setter
+    def discoverable(self, value):
+        self._adapter_obj.Discoverable = value
+        return
+
+    @property
+    def discovering(self):
+        return self._adapter_obj.Discovering
+
+    @property
+    def pairable(self):
+        return self._adapter_obj.Pairable
+
+    @pairable.setter
+    def pairable(self, value):
+        self._adapter_obj.Pairable = value
+        return
+
+    @property
+    def powered(self):
+        return self._adapter_obj.Powered
+
+    @powered.setter
+    def powered(self, value):
+        self._adapter_obj.Powered = value
+        return
+
+    @property
+    def address(self):
+        return self._adapter_obj.Address
+
+    @property
+    def alias(self):
+        return self._adapter_obj.Alias
+
+    @alias.setter
+    def alias(self, value):
+        self._adapter_obj.Alias = value
+        return
+
+    @property
+    def modalias(self):
+        return self._adapter_obj.Modalias
+
+    @property
+    def dbus_name(self):
+        return self._adapter_obj.Name
+
+    @property
+    def dbus_class(self):
+        return self._adapter_obj.Class
+
+    @property
+    def discoverable_timeout(self):
+        return self._adapter_obj.DiscoverableTimeout
+
+    @discoverable_timeout.setter
+    def discoverable_timeout(self, value):
+        self._adapter_obj.DiscoverableTimeout = value
+        return
+
+    @property
+    def pairable_timeout(self):
+        return self._adapter_obj.PairableTimeout
+
+    @pairable_timeout.setter
+    def pairable_timeout(self, value):
+        self._adapter_obj.PairableTimeout = value
+        return
+
+    @classmethod
+    def _find_adapter(cls, hci_address):
+        result = None
+
+        obj_dict = cls.__bluez_root.GetManagedObjects()
+        # Filter only object paths with 'hci'
+        hci_dict = {key: val for (key, val) in obj_dict.items() if 'hci' in key}
+        for path, hci in hci_dict.items():
+            if hci['org.bluez.Adapter1']['Address'] == hci_address:
+                result = SYSTEM_BUS.get(BLUEZ_NAME, path)
+
+        if result is None:
+            raise Exception('No HCI found with address {}'.format(hci_address))
+        return result
+
+
+if __name__ == '__main__':
+    hci0 = Adapter('5C:F3:70:81:D3:6C')
+    print(hci0.powered)
